361699d1ca9849fbaf6e641de13ccc534e6f0b8c
Phil Flanagan <phileflanagan@gmail.com>
2016-04-23 01:24:16 -0400

    added two function
    edit and delete

--- scripts/storeController.js
+++ scripts/storeController.js
@@ -14,7 +14,26 @@ angular
 			var newListingHolder = newListing;
 			$scope.items.push(newListingHolder);
 			$scope.newListing = {};
+			$scope.addListing = false;
 			return $scope.items;
+		}
+
+		$scope.editItem = function(item) {
+			$scope.editListing = true;
+			$scope.existingListing = item;
+
+		}
+
+		$scope.saveStoreEdit = function() {
+			$scope.existingListing = {};
+			$scope.editListing = false;
+		}
+
+		$scope.deleteItem = function(listing) {
+			var index = $scope.items.indexOf(listing);
+			$scope.items.splice(index, 1);
+			$scope.existingListing = {};
+			$scope.editListing = false;
 		}
 
 		storeFactory.getStore().success(function(data){
